d3e617a8bd914f28a0d81831bfa8dde044e80600
lupuscramus <lupuscramus@localdomain.(none)>
2011-11-23 20:51:50 +0100

    L'application staticfiles n'est pas disponible por django 1.2

--- settings.py
+++ settings.py
@@ -72,7 +72,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+	#    'django.contrib.staticfiles',
 	'elections.vote',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
